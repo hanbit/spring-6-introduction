a33104423903058ce27b34ce4071ad2845c78964
Hanbit <webmaster@hanb.co.kr>
2024-03-05 09:51:48 +0900

    Delete 0402-shopping-stereotype-annotation directory

--- 0402-shopping-stereotype-annotation/src/main/java/com/example/shopping/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shopping.repository;
-
-import com.example.shopping.entity.OrderItem;
-
-public interface OrderItemRepository {
-    void insert(OrderItem orderItem);
-}
--- 0402-shopping-stereotype-annotation/src/main/java/com/example/shopping/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.shopping.repository;
-
-import com.example.shopping.entity.Order;
-
-public interface OrderRepository {
-    void insert(Order order);
-}
