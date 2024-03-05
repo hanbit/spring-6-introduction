3d338d3ff6018e3492d13a618f931e04aef5d1aa
Hanbit <webmaster@hanb.co.kr>
2024-03-05 09:51:28 +0900

    Delete 0301-training-di-answer directory

--- 0301-training-di-answer/src/main/java/com/example/training/repository/TrainingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.training.repository;
-
-import java.util.List;
-
-import com.example.training.entity.Training;
-
-public interface TrainingRepository {
-    List<Training> selectAll();
-}
--- 0301-training-di-answer/src/main/java/com/example/training/service/TrainingService.java
+++
@@ -1,9 +0,0 @@
-package com.example.training.service;
-
-import java.util.List;
-
-import com.example.training.entity.Training;
-
-public interface TrainingService {
-    List<Training> findAll();
-}
