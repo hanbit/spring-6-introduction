1ebe1e50a01026d2bbaae5143fab2c53fd91cf88
Hanbit <webmaster@hanb.co.kr>
2024-03-05 09:51:21 +0900

    Delete 0002-shopping-common directory

--- 0002-shopping-common/src/main/java/com/example/shopping/enumeration/PaymentMethod.java
+++
@@ -1,5 +0,0 @@
-package com.example.shopping.enumeration;
-
-public enum PaymentMethod {
-    BANK, CONVENIENCE_STORE
-}
--- 0002-shopping-common/src/main/java/com/example/shopping/exception/StockShortageException.java
+++
@@ -1,8 +0,0 @@
-package com.example.shopping.exception;
-
-@SuppressWarnings("serial")
-public class StockShortageException extends RuntimeException {
-    public StockShortageException(String msg) {
-        super(msg);
-    }
-}
