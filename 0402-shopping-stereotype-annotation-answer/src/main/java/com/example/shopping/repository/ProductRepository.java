c0ea8340eca1a47efdaf3932ecbba9353ece9c1b
Hanbit <webmaster@hanb.co.kr>
2024-03-05 09:51:41 +0900

    Delete 0402-shopping-stereotype-annotation-answer directory

--- 0402-shopping-stereotype-annotation-answer/src/main/java/com/example/shopping/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shopping.repository;
-
-import com.example.shopping.entity.Product;
-
-public interface ProductRepository {
-    Product selectById(String id);
-
-    boolean update(Product product);
-}
